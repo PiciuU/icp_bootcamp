6bb0905b6ff28987a3e521de70f82d28cc7a57ac
Patryk Strzalka <piciuubest@gmail.com>
2024-12-03 16:37:25 +0100

    in progress to add blogs

--- src/chat_backend/src/lib.rs
+++ src/chat_backend/src/lib.rs
@@ -1,20 +1,18 @@
 use std::cell::RefCell;
 
+mod blog;
+
 thread_local! {
-    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
 }
 
-#[ic_cdk::query]
-fn get_chat() -> Vec<String> {
-    CHAT.with(|chat: &RefCell<Vec<String>>| chat.borrow().clone())
-}
 
 #[ic_cdk::update]
-fn add_msg(new_msg: String) {
-    CHAT.with(|chat: &RefCell<Vec<String>>| chat.borrow_mut().push(new_msg))
+fn add_blog(new_blog: String) {
+    BLOGS.with(|blogs: &RefCell<Vec<String>>| blogs.borrow_mut().push(new_blog));
 }
 
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get_blogs() -> Vec<String> {
+    BLOGS.with(|blogs: &RefCell<Vec<String>>| blogs.borrow().clone())
 }
