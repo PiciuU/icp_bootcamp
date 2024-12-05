f629e07d22ca1a9555fee7204599f17a15e9fe65
Patryk Strzalka <piciuubest@gmail.com>
2024-12-05 18:28:39 +0100

    add function to fetch config

--- src/blog_backend/src/lib.rs
+++ src/blog_backend/src/lib.rs
@@ -61,6 +61,11 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
 }
 
 #[ic_cdk::query]
+fn get_config() -> Config {
+    CONFIG.with(|config: &RefCell<Config>| config.borrow().clone())
+}
+
+#[ic_cdk::query]
 fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs: &RefCell<Vec<Blog>>| blogs.borrow().clone())
 }
